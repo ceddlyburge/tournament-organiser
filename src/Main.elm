ea71efa7f308350c1d1d23b94e34fd510f8395a1
Cedd Burge <ceddlyburge@gmail.com>
2021-04-13 16:41:23 +0100

    Add ui to add a new team

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, p, text)
+import Html exposing (Html, button, div, h1, input, p, text)
 import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 
 
 
 ---- MODEL ----
+
+
+type UiState
+    = TeamsView
+    | AddTeamView
+    | GameOrderView
 
 
 type alias Team =
@@ -16,17 +22,21 @@ type alias Team =
 
 
 type alias Model =
-    { teams : List Team
+    { uiState : UiState
+    , teams : List Team
+    , teamNameToAdd : String -- used when adding a new team
     }
 
 
 vanillaModel : Model
 vanillaModel =
     Model
+        TeamsView
         [ Team "Castle"
         , Team "Blackwater"
         , Team "ULU"
         ]
+        ""
 
 
 init : ( Model, Cmd Msg )
@@ -41,6 +51,9 @@ init =
 type Msg
     = NoOp
     | DeleteTeam Team
+    | ShowAddTeam
+    | SetTeamNameToAdd String
+    | AddTeam
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,6 +61,15 @@ update msg model =
     case msg of
         DeleteTeam team ->
             ( { model | teams = List.filter (\t -> t /= team) model.teams }, Cmd.none )
+
+        ShowAddTeam ->
+            ( { model | uiState = AddTeamView }, Cmd.none )
+
+        SetTeamNameToAdd teamName ->
+            ( { model | teamNameToAdd = teamName }, Cmd.none )
+
+        AddTeam ->
+            ( { model | teams = Team model.teamNameToAdd :: model.teams, uiState = TeamsView }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
@@ -68,13 +90,40 @@ view model =
             ]
         , Html.section
             []
-            (teamsView model)
+            (stateView model)
         ]
+
+
+stateView : Model -> List (Html Msg)
+stateView model =
+    case model.uiState of
+        TeamsView ->
+            teamsView model
+
+        AddTeamView ->
+            addTeamView
+
+        _ ->
+            []
+
+
+addTeamView : List (Html Msg)
+addTeamView =
+    [ input
+        [ onInput SetTeamNameToAdd ]
+        []
+    , button
+        [ onClick AddTeam ]
+        [ text "Add Team" ]
+    ]
 
 
 teamsView : Model -> List (Html Msg)
 teamsView model =
-    List.map teamView model.teams
+    button
+        [ onClick ShowAddTeam ]
+        [ text "Add Team" ]
+        :: List.map teamView model.teams
 
 
 teamView : Team -> Html Msg
